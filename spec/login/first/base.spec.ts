53b46a0cebd6c36715e186ecf044fd20b0f7afd6
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-06-22 11:42:03 +0300

    Added first login content test

---
+++ spec/login/first/base.spec.ts
@@ -0,0 +1,28 @@
+import {assemblerInterface} from "../../../src/assembler";
+
+
+describe('First login', () => {
+  describe('Base', () => {
+
+    const service: assemblerInterface = (jasmine.getEnv() as any).service;
+    let anyUser = null;
+
+    beforeAll(async () => {
+      await service.common.app.relaunch();
+      anyUser = service.common.user.any().allocate();
+    });
+    afterAll(() => anyUser.free());
+
+    it('page content is displayed', async () => {
+      expect(await service.login.first.page.contentIsDisplayed())
+        .toBe(true, 'Some page content is missing');
+    });
+
+    it('successful login', async () => {
+      await service.login.first.as(anyUser);
+      expect(await service.home.page.isOpen())
+        .toBe(true, `Landing page didn't get opened`);
+    });
+
+  });
+});
