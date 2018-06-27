e40d32ad5280b41028583727d23c29359a4c66cc
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-06-27 13:48:26 +0300

    Added feature level to test selector; now saving all choices instead of the first one

--- src/helpers/testsSelector.helper.ts
+++ src/helpers/testsSelector.helper.ts
@@ -14,18 +14,19 @@ async function selectTests(): Promise<string[]> {
   try {
 
     const features = getFeatures();
+    const featurePromptOptions = getPromptObj(features);
     const selectedFeature = await selectPrompt({
       question: 'Select feature:',
-      options: getPromptObj(features)
+      options: featurePromptOptions
     });
-    if (features.indexOf(selectedFeature) === readRememberedInput(featureChoiceNumberPath)) {
+    if (features.indexOf(selectedFeature) === +readRememberedInput(featureChoiceNumberPath)[0]) {
       selectedFeatureChangedFromLastRun = false;
     }
     fs.writeFileSync(featureChoiceNumberPath, features.indexOf(selectedFeature));
 
 
     const testsPaths = fsHelper.getFiles(`${specsPath}/${selectedFeature}`);
-    const promptOptions = getPromptObj(testsPaths);
+    const promptOptions = getPromptObj(testsPaths).filter(specs);
     selectedFeatureChangedFromLastRun || preselectLastInput(promptOptions);
     return await multiselectPrompt({
       question: 'Select tests to run',
@@ -55,12 +56,13 @@ function getPromptObj(arr) {
 
 function selectPrompt(params): Promise<string> {
   const {question, options} = params;
-  console.info(`Choose nothing to go with everything`);
   return new Promise(resolve => {
-    const rememberedInput = readRememberedInput(featureChoiceNumberPath);
-    const cursor = rememberedInput === 0 ? 0 : rememberedInput || options.length / 2;
+    const rememberedInput = +readRememberedInput(featureChoiceNumberPath)[0];
+    const cursor = rememberedInput >= 0 ? rememberedInput : options.length / 2;
+    console.info('Press esc to choose everything');
     prompt(question, options, {cursor})
-      .on('submit', resolve);
+      .on('submit', resolve)
+      .on('abort', () => resolve(''));
   });
 }
 
@@ -69,16 +71,20 @@ function multiselectPrompt(params): Promise<string[]> {
   console.info(`Choose nothing to go with everything`);
   return new Promise(resolve => {
     const rememberedInput = readRememberedInput(testChoiceNumberPath);
-    const cursor = selectedFeatureChangedFromLastRun
-      ? options.length / 2 :
-      rememberedInput === 0 ? 0 : rememberedInput || options.length / 2;
+    const cursor = selectedFeatureChangedFromLastRun || rememberedInput.length === 0
+      ? options.length / 2
+      : rememberedInput[rememberedInput.length / 2];
     multiPrompt(`${question}:`, options, {cursor})
       .on('submit', items => {
-        getSelectedItemsValues(items).length === 0 && markAllItemsSelected(items);
+        if (getSelectedItemsValues(items).length === 0) {
+          writeTestsInput(items);
+          markAllItemsSelected(items);
+        } else {
+          writeTestsInput(items);
+        }
         const selected = getSelectedItemsValues(items);
         console.info(`${resultMessage}: `);
         logChoices(selected);
-        writeTestsInput(items);
         resolve(selected);
       });
   });
@@ -100,20 +106,20 @@ function getSelectedItemsValues(items) {
 }
 
 function writeTestsInput(items) {
-  items.find((item, index) => {
-    if (item.selected) {
-      fs.writeFileSync(testChoiceNumberPath, index);
-      return true;
-    }
+  const indexes = [];
+  items.forEach((item, index) => {
+    item.selected && indexes.push(index);
   });
+  fs.writeFileSync(testChoiceNumberPath, indexes);
 }
 
 function readRememberedInput(path) {
   try {
-    return +fs.readFileSync(path);
+    const rememberedInput = fs.readFileSync(path).toString();
+    return rememberedInput ? rememberedInput.split(',') : [];
   } catch (err) {
     if (err.message.includes('ENOENT')) {
-      return null;
+      return [];
     } else {
       throw err;
     }
@@ -121,13 +127,15 @@ function readRememberedInput(path) {
 }
 
 function preselectLastInput(items) {
-  const lastInput = readRememberedInput(testChoiceNumberPath);
-  if (lastInput === 0 || lastInput) {
-    items[lastInput].selected = true;
-  }
+  const lastInputs = readRememberedInput(testChoiceNumberPath);
+  lastInputs.forEach(lastInput => items[lastInput].selected = true);
 }
 
 function getFeatures() {
   return fs.readdirSync(specsPath)
     .filter(str => !str.includes('.'));
+}
+
+function specs(file) {
+  return file.title.includes('.spec');
 }
