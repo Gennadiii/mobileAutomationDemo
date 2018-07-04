7377832384bf721b6f1b0bea1174fcd37d6a7075
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-07-04 09:11:36 +0300

    Added smoke option to tests selector; added android 4.1.21

--- src/helpers/testsSelector.helper.ts
+++ src/helpers/testsSelector.helper.ts
@@ -7,32 +7,38 @@ const fs = require('fs');
 const specsPath = `${__dirname}/../../spec/`;
 const testChoiceNumberPath = `${__dirname}/testChoiceNumber.indexHelper`;
 const featureChoiceNumberPath = `${__dirname}/featureChoiceNumber.indexHelper`;
+const testsCollections = {
+  smoke: 'smoke',
+  otherThanSmoke: 'other than smoke',
+};
 let selectedFeatureChangedFromLastRun = true;
+let testsPaths = null;
 
 
 async function selectTests(): Promise<string[]> {
   try {
 
-    const features = getFeatures();
+    const features = [testsCollections.otherThanSmoke, testsCollections.smoke]
+      .concat(getFeatures());
     const featurePromptOptions = getPromptObj(features);
-    const selectedFeature = await selectPrompt({
-      question: 'Select area:',
-      options: featurePromptOptions
-    });
+    const selectedFeature = await promptFeature(featurePromptOptions);
+
     if (features.indexOf(selectedFeature) === +readRememberedInput(featureChoiceNumberPath)[0]) {
       selectedFeatureChangedFromLastRun = false;
     }
-    fs.writeFileSync(featureChoiceNumberPath, features.indexOf(selectedFeature));
+    writeFeatureInput(features, selectedFeature);
 
 
-    const testsPaths = fsHelper.getFiles(`${specsPath}/${selectedFeature}`);
+    if (selectedFeature === testsCollections.smoke) {
+      testsPaths = fsHelper.getFiles(`${specsPath}`).filter(base);
+    } else if (selectedFeature === testsCollections.otherThanSmoke) {
+      testsPaths = fsHelper.getFiles(`${specsPath}`).filter(otherThanBase);
+    } else {
+      testsPaths = fsHelper.getFiles(`${specsPath}/${selectedFeature}`);
+    }
     const promptOptions = getPromptObj(testsPaths).filter(specs);
     selectedFeatureChangedFromLastRun || preselectLastInput(promptOptions);
-    return await multiselectPrompt({
-      question: 'Select tests to run',
-      resultMessage: 'Running tests',
-      options: promptOptions
-    });
+    return await promptTests(promptOptions);
 
   } catch (err) {
     console.error(`Can't start tests: ${err}`);
@@ -54,27 +60,25 @@ function getPromptObj(arr) {
   });
 }
 
-function selectPrompt(params): Promise<string> {
-  const {question, options} = params;
+function promptFeature(options): Promise<string> {
   return new Promise(resolve => {
     const rememberedInput = +readRememberedInput(featureChoiceNumberPath)[0];
     const cursor = rememberedInput >= 0 ? rememberedInput : Math.floor(options.length / 2);
     console.info('Press esc to choose everything');
-    prompt(question, options, {cursor})
+    prompt('Select area:', options, {cursor})
       .on('submit', resolve)
       .on('abort', () => resolve(''));
   });
 }
 
-function multiselectPrompt(params): Promise<string[]> {
-  const {question, resultMessage, options} = params;
+function promptTests(options): Promise<string[]> {
   console.info(`Choose nothing to go with everything`);
   return new Promise(resolve => {
     const rememberedInput = readRememberedInput(testChoiceNumberPath);
     const cursor = selectedFeatureChangedFromLastRun || rememberedInput.length === 0
       ? Math.floor(options.length / 2)
       : rememberedInput[Math.floor(rememberedInput.length / 2)];
-    multiPrompt(`${question}:`, options, {cursor})
+    multiPrompt('Select tests to run: ', options, {cursor})
       .on('submit', items => {
         if (getSelectedItemsValues(items).length === 0) {
           writeTestsInput(items);
@@ -83,7 +87,7 @@ function multiselectPrompt(params): Promise<string[]> {
           writeTestsInput(items);
         }
         const selected = getSelectedItemsValues(items);
-        console.info(`${resultMessage}: `);
+        console.info(`Running tests: `);
         logChoices(selected);
         resolve(selected);
       });
@@ -113,6 +117,10 @@ function writeTestsInput(items) {
   fs.writeFileSync(testChoiceNumberPath, indexes);
 }
 
+function writeFeatureInput(features, selectedFeature) {
+  fs.writeFileSync(featureChoiceNumberPath, features.indexOf(selectedFeature));
+}
+
 function readRememberedInput(path) {
   try {
     const rememberedInput = fs.readFileSync(path).toString();
@@ -138,4 +146,12 @@ function getFeatures() {
 
 function specs(file) {
   return file.title.includes('.spec');
+}
+
+function base(file) {
+  return file.includes('base');
+}
+
+function otherThanBase(file) {
+  return !file.includes('base');
 }
