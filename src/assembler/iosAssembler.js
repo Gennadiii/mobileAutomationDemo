367e91801020a53df4fd6fb7df025152b36703a8
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-07-05 10:53:13 +0300

    Deleted js files

--- src/assembler/iosAssembler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var helper_1 = require("../helpers/helper");
-var elementFinder_helper_1 = require("../helpers/element_finder/elementFinder.helper");
-var index_1 = require("../../index");
-var genericServices_1 = require("./genericServices");
-var elementFinder = new elementFinder_helper_1.ElementFinder('name');
-var iosServices = genericServices_1.assembleServices(elementFinder, helper_1.helper.lib.all, index_1.driver);
-exports.iosServices = iosServices;
