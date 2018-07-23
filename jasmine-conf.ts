b4383dd3be6d36f39909e477f9715415871c61d9
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-07-23 10:43:46 +0300

    XML reporter; ios build

--- jasmine-conf.ts
+++ jasmine-conf.ts
@@ -4,20 +4,30 @@ import {helper} from "./src/helpers/helper";
 const Jasmine = require('jasmine');
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 const HtmlReporter = require('jasmine-pretty-html-reporter').Reporter;
+const jasmineReporters = require('jasmine-reporters');
 const mkdirp = require('mkdirp');
 
 
 const log = helper.logger.get('jasmine');
 const jasmine = new Jasmine();
 jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 2 * 60 * 1000;
+
+
 const specReporter = new SpecReporter({
   spec: {
     displayPending: true
   }
 });
+
 const htmlReporter = new HtmlReporter({
-  path: 'report'
+  path: 'reports'
 });
+
+const junitReporter = new jasmineReporters.JUnitXmlReporter({
+  savePath: 'reports',
+  consolidateAll: false,
+});
+
 let suiteStartTime = null;
 const screenshotReporter = {
   jasmineStarted() {
@@ -34,6 +44,8 @@ const screenshotReporter = {
     }
   },
 };
+
+
 jasmine.loadConfig({
   spec_dir: 'dist/spec',
   random: false,
@@ -43,6 +55,7 @@ jasmine.loadConfig({
 
 jasmine.addReporter(specReporter);
 jasmine.addReporter(htmlReporter);
+jasmine.addReporter(junitReporter);
 jasmine.addReporter(screenshotReporter);
 
 
